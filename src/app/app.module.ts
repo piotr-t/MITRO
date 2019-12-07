1f6d578e1eb93f8ffe5912d0372920d1625912fc
piotr-t <troc.piotr@gmail.com>
2019-12-07 17:52:55 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [/* {provide: LocationStrategy, useClass: HashLocationStrategy} */],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
